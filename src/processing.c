0bc68f52b2af69d00a984d4a11a7ab26cb1e4de8
Soeking <great.from.g@gmail.com>
2020-01-08 19:03:40 +0900

    fix bug

--- src/processing.c
+++ src/processing.c
@@ -28,7 +28,7 @@ int searchId(str *ss, int id, int size) {
     return -1;
 }
 
-int loopBm(char *text, const char *outText, const int *used, str s, int l, int index, int strId) {
+int loopBm(char *text, const char *outText, str s, int l, int index) {
     while (true) {
         index = bm(text, s.s, s.table, index, l, s.len);
         if (index < 0) break;
@@ -40,17 +40,28 @@ int loopBm(char *text, const char *outText, const int *used, str s, int l, int i
     return index;
 }
 
+int loopSoft(char *text, str s, int l, int index) {
+    while (true) {
+        index = bm(text, s.s, s.table, index, l, s.len);
+        if (index < 0) break;
+        if ((s.s[0] == 'c' && text[index - 1] == 'd') ||
+            (s.s[s.len - 1] == 'd' && text[index + s.len] == 'c'))
+            index += s.len;
+        else break;
+    }
+    return index;
+}
+
 void procStr(char *text, char *outText, int *used, int i, str *strs, int l, int size) {
-    int index = loopBm(text, outText, used, strs[i], l, strs[i].len + strs[i].id, i);
+    int index = loopBm(text, outText, strs[i], l, strs[i].len + strs[i].id);
     while (true) {
         if (index < 0) break;
-        if (used[index] >= 0 && used[index + strs[i].len - 1] >= 0) break;
-        index = loopBm(text, outText, used, strs[i], l, index + strs[i].len, i);
+        if (used[index] < 0 && used[index + strs[i].len - 1] < 0) break;
+        index = loopBm(text, outText, strs[i], l, index + strs[i].len);
     }
     if (index < 0) {
-        int first = loopBm(text, outText, used, strs[i], l, strs[i].len + strs[i].id, i);
-        if (first < 0) first = loopBm(text, outText, used, strs[i], l, strs[i].len - 1, i); //多分あった方がいい(精度落ちたけど)
-        if (first < 0) return;
+        int first = loopBm(text, outText, strs[i], l, strs[i].len + strs[i].id);
+        if (first < 0) first = loopBm(text, outText, strs[i], l, strs[i].len - 1);
         int idf = searchId(strs, first, size);
         int idl = searchId(strs, first + strs[i].len - 1, size);
         if (idf > 0 && i != idf) revert(text, outText, used, strs[idf].id, strs[idf].len);
@@ -72,6 +83,26 @@ void procStr(char *text, char *outText, int *used, int i, str *strs, int l, int 
     }
 }
 
+void lastProc(char *text, char *outText, int *used, int i, str *strs, int l, int size) {
+    int first = loopSoft(text, strs[i], l, strs[i].len + strs[i].id);
+    if (first < 0) first = loopSoft(text, strs[i], l, strs[i].len - 1);
+    int idf = searchId(strs, first, size);
+    int idl = searchId(strs, first + strs[i].len - 1, size);
+    if (idf > 0 && i != idf) revert(text, outText, used, strs[idf].id, strs[idf].len);
+    if (idl > 0 && idf != idl && i != idl) revert(text, outText, used, strs[idl].id, strs[idl].len);
+
+    strs[i].id = first;
+    put(outText, used, strs[i].s, strs[i].id, strs[i].len, i);
+    if (idf > 0) {
+        if (i != idf)
+            procStr(text, outText, used, idf, strs, l, size);
+    }
+    if (idl > 0 && idf != idl) {
+        if (i != idl)
+            procStr(text, outText, used, idl, strs, l, size);
+    }
+}
+
 void proc(char *text, str *strs, int size, FILE *out) {
     int l = (int) strlen(text);
     char *outText;
@@ -81,15 +112,20 @@ void proc(char *text, str *strs, int size, FILE *out) {
     used = (int *) malloc(sizeof(int) * (size_t) l);
     memset(used, -1, (size_t) l);
     for (int i = 0; i < l; ++i) used[i] = -1;
-    for (int i = 0; i < size; ++i) {
-        if (strs[i].len < 20) break;
+    int i;
+    for (i = 0; i < size; ++i) {
+        if (strs[i].len < 14) break;
         tableInit(strs[i].table, strs[i].s, strs[i].len);
         procStr(text, outText, used, i, strs, l, size);
     }
 
-//    for (int i = 0; i < l; ++i) {
-//        if (outText[i] == 'x') fprintf(out, "a");
-//        else fprintf(out, "%c", outText[i]);
-//    }
-    fprintf(out, "%s\n", outText);
+    for (int j = 0; j < i; ++j) {
+        if (strs[i].id == -1) lastProc(text, outText, used, i, strs, l, size);
+    }
+
+    for (int j = 0; j < l; ++j) {
+        if (outText[j] == 'x') fprintf(out, "a");
+        else fprintf(out, "%c", outText[j]);
+    }
+    fprintf(out, "\n");
 }
